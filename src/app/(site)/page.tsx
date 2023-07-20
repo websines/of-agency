382713a1406c020cf89baf85d08774a69a78c11f
websines <to.subh03@gmail.com>
2023-07-20 20:33:24 +0530

    preline

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,8 +1,13 @@
+"use client";
 import BlogsSection from "@/components/BlogsSection";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    import("preline");
+  }, []);
   return (
     <main className="bg-white">
       <section id="slide-1" className="w-full min-h-screen relative">
