a9cf725b59943f5e89d182c9ad2756d449bb85cb
websines <to.subh03@gmail.com>
2023-07-21 15:09:00 +0530

    changed auth

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,9 +4,9 @@ export default function Page() {
   return (
     <div className="p-8 sm:p-16 min-h-screen flex flex-col justify-center items-center">
       <div className="my-6">
-        <h1 className="text-xl font-bold">Passwordless Auth</h1>
+        <h1 className="text-xl font-bold">Log In to Your Dashboard</h1>
       </div>
-      <div className="sm:w-1/2 p-4">
+      <div className="sm:w-1/2 w-full p-4">
         <AuthForm />
       </div>
     </div>
--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -2,20 +2,32 @@
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { Database } from "@/lib/supabase";
-import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import {
+  useSessionContext,
+  useSupabaseClient,
+} from "@supabase/auth-helpers-react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function AuthForm() {
   const supabaseClient = useSupabaseClient();
+  const router = useRouter();
+  const { session } = useSessionContext();
+
+  useEffect(() => {
+    if (session) {
+      router.push("/dashboard");
+    }
+  }, [session, router]);
 
   return (
     <Auth
       supabaseClient={supabaseClient}
-      view="magic_link"
+      magicLink
       appearance={{ theme: ThemeSupa }}
       theme="light"
-      showLinks={false}
       providers={[]}
-      redirectTo={`${process.env.PROJECT_URL}/auth/callback`}
+      redirectTo="/auth/callback"
     />
   );
 }
