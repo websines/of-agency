1a3e5a305183b77b8aec31c9f70ad06b323dd2e1
websines <to.subh03@gmail.com>
2023-07-21 15:34:10 +0530

    bg-white

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,7 +2,7 @@ import AuthForm from "@/components/AuthForm";
 
 export default function Page() {
   return (
-    <div className="p-8 sm:p-16 min-h-screen flex flex-col justify-center items-center">
+    <div className="p-8 sm:p-16 min-h-screen flex flex-col justify-center items-center bg-white">
       <div className="my-6">
         <h1 className="text-xl font-bold">Log In to Your Dashboard</h1>
       </div>
